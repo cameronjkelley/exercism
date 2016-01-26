3b87ca010cb4bb8b54676b15152f711a571cf24e
Cameron Kelley <i.am.cjk@gmail.com>
2016-01-26 18:44:43 -0500

    Update rna_transcription.js

--- javascript/rna-transcription/rna_transcription.js
+++ javascript/rna-transcription/rna_transcription.js
@@ -1,26 +1,27 @@
 var DNA_COMPLEMENTS = { "U": "A", "C": "G", "G": "C", "A": "T"},
   	RNA_COMPLEMENTS = { "A": "U", "C": "G", "G": "C", "T": "A" },
-	  DnaTranscriber = function() {};
+	DnaTranscriber = function() {};
 
 DnaTranscriber.prototype.toRna = function(dna) {
-	transcribe(dna, RNA_COMPLEMENTS);
+	return transcribe(dna, RNA_COMPLEMENTS);
 };
 
 DnaTranscriber.prototype.toDna = function(rna) {
-	transcribe(rna, DNA_COMPLEMENTS);
+	return transcribe(rna, DNA_COMPLEMENTS);
 };
 
-function transcribe(strand, complement) {
-	
+function transcribe(strand, complement) {	
 	var bases = strand.split(""),
-			keys = Object.keys(complement),
-			result = "";
+		result = "";
 
-	for (var i = 0; i < bases.length; i++) {
-
-		if complementbases[i]
+	for(var i = 0; i < bases.length; i++) {
+		for(var key in complement) {
+			if(bases[i] == key) {
+				result += complement[key];
+			}
+		}
 	}
-
+	return result;
 };
 
 module.exports = DnaTranscriber;
