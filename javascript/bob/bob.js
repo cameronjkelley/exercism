c8c7ccbcb1094e21e048ddd8d83f7e5b75fd001d
Cameron Kelley <i.am.cjk@gmail.com>
2016-01-29 15:46:09 -0500

    Create bob.js

---
+++ javascript/bob/bob.js
@@ -0,0 +1,27 @@
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+	if (yelling(input)) {
+		return "Whoa, chill out!";
+	} else if (asking(input)) {
+		return "Sure.";
+	} else if (telling(input)) {
+		return "Whatever.";
+	} else {
+		return"Fine. Be that way!";
+	}
+};
+
+function asking(input) {
+	return input.slice(-1) === "?";
+};
+
+function telling(input) {
+	return input.trim().length > 0;
+};
+
+function yelling(input) {
+	return input.toUpperCase() === input && input.toLowerCase() !== input;
+};
+
+module.exports = Bob;
