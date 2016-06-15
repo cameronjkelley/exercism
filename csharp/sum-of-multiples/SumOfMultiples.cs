0164ea2f5f1f314c40cdff1265a330fe7b67088d
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:26:26 -0400

    First Iteration

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class SumOfMultiples
+{
+  public static int To(int[] multiples, int limit)
+  {
+    List<int> result = new List<int>();
+    IEnumerable<int> range = Enumerable.Range(1, limit - 1);
+
+    foreach (int x in range)
+    {
+      foreach (int y in multiples)
+      {
+        if (x % y == 0 && !result.Contains(x))
+        {
+          result.Add(x);
+        }
+      }
+    }
+    return result.Sum();
+  }
+}
