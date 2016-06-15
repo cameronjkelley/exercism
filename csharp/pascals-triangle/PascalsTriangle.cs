8dd3ee15c5a9776170cf078806d441f8af31469a
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:14:01 -0400

    First Iteration

---
+++ csharp/pascals-triangle/PascalsTriangle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public class PascalsTriangle
+{
+  public static List<List<int>> Calculate(int number)
+  {
+    List<List<int>> rows = new List<List<int>>();
+    rows.Add(new List<int>() { 1 });
+    for (int i = 1; i < number; i++)
+    {
+      List<int> row = new List<int>();
+      row.Add(1);
+      for (int j = 1; j < i; j++)
+      {
+        if (i > 1)
+        {
+          row.Add(rows[i - 1][j - 1] + rows[i - 1][j]);
+        }
+      }
+      row.Add(1);
+      rows.Add(row);
+    }
+    return rows;
+  }
+}
