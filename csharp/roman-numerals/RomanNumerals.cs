9406b66881ec50bb328e684303da7567fd53a30f
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:21:31 -0400

    First Iteration

---
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+public static class RomanNumerals
+{
+  private static Dictionary<int, string> Numerals = new Dictionary<int, string>()
+  {
+    { 1000, "M" },
+    { 900, "CM" },
+    { 500, "D" },
+    { 400, "CD" },
+    { 100, "C" },
+    { 90, "XC" },
+    { 50, "L" },
+    { 40, "XL" },
+    { 10, "X" },
+    { 9, "IX" },
+    { 5, "V" },
+    { 4, "IV" },
+    { 1, "I" }
+  };
+
+  public static string ToRoman(this int input)
+  {
+    string output = "";
+    foreach (var pair in Numerals)
+    {
+      while (input / pair.Key > 0)
+      {
+        output += Numerals[pair.Key];
+        input -= pair.Key;
+      }
+    }
+    return output;
+  }
+}
