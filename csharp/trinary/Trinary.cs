5ad070e329d474db9470ce2dff2895e18f29e96b
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:28:04 -0400

    First Iteration

---
+++ csharp/trinary/Trinary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Trinary
+{
+  public static int ToDecimal(string number)
+  {
+    int result = 0;
+    if (IsValidTrinary(number))
+    {
+      string reversed = Reverse(number);
+      for (int i = 0; i < reversed.Length; i++)
+      {
+        result += int.Parse(reversed[i].ToString()) * (int)Math.Pow(3, i);
+      }
+    }
+    return result;
+  }
+
+  private static bool IsValidTrinary(string number)
+  {
+    bool result = false;
+    Regex pattern = new Regex(@"^[012]+$");
+    if (pattern.IsMatch(number))
+    {
+      result = true;
+    }
+    return result;
+  }
+
+  private static string Reverse(string s)
+  {
+    char[] charArray = s.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+  }
+}
