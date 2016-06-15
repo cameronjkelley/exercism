34df51d71b2414de097e55d84ab9c62b44ba77d8
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:11:47 -0400

    First Iteration

---
+++ csharp/nucleotide-count/DNA.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DNA
+{
+  public DNA(string input)
+  {
+    SetNucleotideCounts(input);
+  }
+
+  public Dictionary<char, int> NucleotideCounts { get; set; }
+
+  private void SetNucleotideCounts(string strand)
+  {
+    NucleotideCounts = new Dictionary<char, int> { { 'A', 0 }, { 'T', 0 }, { 'C', 0 }, { 'G', 0 } };
+
+    foreach(var x in strand)
+    {
+      NucleotideCounts[x] += 1;
+    }
+  }
+
+  public int Count(char input)
+  {
+    int count;
+    if (!NucleotideCounts.TryGetValue(input, out count))
+    {
+      throw new InvalidNucleotideException();
+    }
+    return count;
+  }
+}
+
+public class InvalidNucleotideException : Exception { }
