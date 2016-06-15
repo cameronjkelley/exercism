1a43ed628657c803718898e870aebc2b1dc8f651
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:19:33 -0400

    First Iteration

---
+++ csharp/rna-transcription/Complement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Complement
+{
+  private static Dictionary<string, string> Nucleotides = new Dictionary<string, string>()
+  {
+    { "G", "C" },
+    { "C", "G" },
+    { "T", "A" },
+    { "A", "U" }
+  };
+
+  public static string OfDna(string input)
+  {
+    return string.Join("", input.Select(x => Nucleotides[x.ToString()]));
+  }
+}
