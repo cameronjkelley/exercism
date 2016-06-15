3bcea89c9745e805acb0ddc5a49f91ce6e474bfb
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 09:52:53 -0400

    Add files via upload

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+public class Acronym
+{
+  public static string Abbreviate(string input)
+  {
+    if (string.IsNullOrEmpty(input)) return string.Empty;
+
+    string output = "";
+
+    Regex pattern = new Regex(@"\b[A-Za-z]|[A-Z](?=[a-z])");
+    Match match = pattern.Match(input);
+
+    while (match.Success)
+    {
+      output += match.Value;
+      match = match.NextMatch();
+    }
+    return output.ToUpper();
+  }
+}
