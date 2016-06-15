f5a6efc1a5482ec85d65efef1b11df5b7d5ba1b3
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:10:19 -0400

    First Iteration

---
+++ csharp/meetup/Meetup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum Schedule
+{
+  Teenth,
+  First,
+  Second,
+  Third,
+  Fourth,
+  Last
+}
+
+public class Meetup
+{
+  private int _month;
+  private int _year;
+
+  public Meetup(int month, int year)
+  {
+    _month = month;
+    _year = year;
+  }
+
+  public DateTime Day(DayOfWeek dow, Schedule preference)
+  {
+    if (preference == Schedule.Last)
+    {
+      return GetLastDay(_year, _month, dow);
+    }
+    return GetPreferredDay(_year, _month, dow, preference);
+  }
+
+  private DateTime GetLastDay(int year, int month, DayOfWeek day)
+  {
+    for (int i = DateTime.DaysInMonth(year, month); i >= 22; i--)
+    {
+      DateTime dt = new DateTime(year, month, i);
+      if (dt.DayOfWeek == day)
+      {
+        return dt;
+      }
+    }
+    return new DateTime();
+  }
+
+  private DateTime GetPreferredDay(int year, int month, DayOfWeek day, Schedule preference)
+  {
+    DateTime date = new DateTime();
+    int idx, limit;
+    if (preference == Schedule.Teenth)
+    {
+      idx = 13;
+      limit = 19;
+    }
+    else
+    {
+      idx = ((int)preference - 1) * 7 + 1;
+      limit = (int)preference * 7;
+    }
+
+    for (; idx <= limit; idx++)
+    {
+      DateTime dt = new DateTime(year, month, idx);
+      if (dt.DayOfWeek == day)
+      {
+        date = dt;
+      }
+    }
+    return date;
+  }
+}
