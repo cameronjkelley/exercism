dbd0fd98a7cd506d21909b736c0031906f0bac1e
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 09:51:46 -0400

    Add files via upload

---
+++ csharp/accumulate/Accumulate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class AccumulateFunction
+{
+  public static IEnumerable<T> Accumulate<T>(this IEnumerable<T> collection, Func<T, T> func)
+  {
+    foreach (var item in collection)
+    {
+      yield return func(item);
+    }
+  }
+}
