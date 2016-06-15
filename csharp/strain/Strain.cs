c6cdff419995cf7f50f4bfffa3ec015a7ecdcb38
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:25:30 -0400

    First Iteration

---
+++ csharp/strain/Strain.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+public static class Strain
+{
+  public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+  {
+    return Filter(collection, predicate);
+  }
+
+  public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+  {
+    return Filter(collection, x => !predicate(x));
+  }
+
+  // From C# in Depth (http://csharpindepth.com/Articles/Chapter5/Closures.aspx)
+  private static IEnumerable<T> Filter<T>(IEnumerable<T> source, Func<T, bool> predicate)
+  {
+    List<T> result = new List<T>();
+    foreach (T item in source)
+    {
+      if (predicate(item))
+      {
+        result.Add(item);
+      }
+    }
+    return result;
+  }
+}
