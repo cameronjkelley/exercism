f172883f037000743eac3fb9773693f203b334dd
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 09:54:58 -0400

    First Iteration

---
+++ csharp/anagram/Anagram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Anagram
+{
+  private string detector;
+
+  public Anagram(string input)
+  {
+    detector = input;
+  }
+
+  private string SortString(string input)
+  {
+    return string.Concat(input.ToLower().OrderBy(x => x));
+  }
+
+  public string[] Match(string[] words)
+  {
+    List<string> matches = new List<string>();
+
+    string sortedDetector = SortString(detector);
+
+    foreach (string word in words)
+    {
+      string lowered = word.ToLower();
+
+      if (SortString(word) == sortedDetector && !matches.Contains(lowered) && lowered != detector)
+      {
+        matches.Add(word);
+      }
+    }
+    return matches.ToArray();
+  }
+}
