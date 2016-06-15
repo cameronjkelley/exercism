7f72de7a5b759d3a13c359d154ff6cb7e313486e
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 09:57:43 -0400

    First Iteration

---
+++ csharp/binary/Binary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Binary
+{
+  public static int ToDecimal(string binary)
+  {
+    int output = 0, i = 0;
+    for (int len = binary.Length - 1; len >= 0; len--)
+    {
+      if (!ValidInput(binary[i]))
+      {
+        return 0;
+      }
+      output += Int32.Parse(binary[i].ToString()) * (int)Math.Pow(2, len);
+      i++;
+    }
+    return output;
+  }
+
+  private static bool ValidInput(char input)
+  {
+    if (input == '0' || input == '1')
+    {
+      return true;
+    }
+    return false;
+  }
+}
