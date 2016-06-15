ffeed15f62e53d60e558fe34d608571f8e0e3fe8
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 14:42:36 -0400

    Improved readability

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -4,12 +4,9 @@
   {
     string result = "";
 
-    if (input % 3 == 0)
-      result += "Pling";
-    if (input % 5 == 0)
-      result += "Plang";
-    if (input % 7 == 0)
-      result += "Plong";
+    if (input % 3 == 0) result += "Pling";
+    if (input % 5 == 0) result += "Plang";
+    if (input % 7 == 0) result += "Plong";
 
     return string.IsNullOrEmpty(result) ? input.ToString() : result;
   }
