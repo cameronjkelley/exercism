748fb77c76f88feaa487f9944caf7b3b48a8074b
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:16:28 -0400

    First Iteration

---
+++ csharp/prime-factors/PrimeFactors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+public class PrimeFactors
+{
+  public static List<int> For(long n)
+  {
+    List<int> primes = new List<int>();
+    int i = 2;
+    while (n > 1)
+    {
+      while (n % i == 0)
+      {
+        primes.Add(i);
+        n /= i;
+      }
+      i++;
+    }
+    return primes;
+  }
+}
