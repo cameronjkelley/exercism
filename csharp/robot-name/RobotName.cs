397ab9f6efdffd5d6e40b7b80f7e87d3b6a08bce
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:20:09 -0400

    First Iteration

---
+++ csharp/robot-name/RobotName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Robot
+{
+  public string Name { get; private set; }
+
+  public Robot()
+  {
+    Reset();
+  }
+
+  public void Reset()
+  {
+    Name = RobotName();
+  }
+
+  private string RobotName()
+  {
+    return GetLetter() + GetLetter() + GetNumbers();
+  }
+
+  private static Random random = new Random();
+
+  private string GetLetter()
+  {
+    return ((char)('A' + random.Next(26))).ToString();
+  }
+
+  private string GetNumbers()
+  {
+    return (random.Next(1000)).ToString("000"); 
+  }
+}
