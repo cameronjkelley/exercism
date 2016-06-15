54f502801f5eb7fc8c2e71577f5549f8d611444d
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:05:04 -0400

    First Iteration

---
+++ csharp/hamming/Hamming.cs
@@ -0,0 +1,17 @@
+﻿public class Hamming
+{
+  public static int Compute(string x, string y)
+  {
+    int diff = 0;
+
+    for (int i = 0; i < x.Length; i++)
+    {
+      if (x[i] != y[i])
+      {
+        diff++;
+      }
+    }
+
+    return diff;
+  }
+}
