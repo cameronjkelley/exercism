5a7424430790e4f0f059fa07f557cbe75f7362bf
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:23:37 -0400

    First Iteration

---
+++ csharp/series/Series.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Series
+{
+  private string _series;
+
+  public Series(string input)
+  {
+    _series = input;
+  }
+
+  public int[][] Slices(int sliceLength)
+  {
+    if (sliceLength > _series.Length)
+    {
+      throw new ArgumentException("The length of a slice cannot exceed the length of the series.");
+    }
+
+    var result = new List<int[]>();
+
+    for (int i = 0; i + sliceLength - 1 < _series.Length; i++)
+    {
+      result.Add(SplitSeries(_series.Substring(i, sliceLength)));
+    }
+    return result.ToArray();
+  }
+
+  private int[] SplitSeries(string input)
+  {
+    return input.Select(x => int.Parse(x.ToString())).ToArray();
+  }
+}
