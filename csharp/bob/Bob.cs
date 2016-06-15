dab9c5330487fb25b5ed40264d43c1ce30accc3d
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 09:58:28 -0400

    First Iteration

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Bob
+{
+  public static string Hey(string input)
+  {
+    if (Silent(input))
+    {
+      return "Fine. Be that way!";
+    }
+    if (Yelling(input))
+    {
+      return "Whoa, chill out!";
+    }
+    if (Asking(input))
+    {
+      return "Sure.";
+    }
+    return "Whatever.";
+  }
+
+  public static bool Silent(string input)
+  {
+    return input.Trim() == "";
+  }
+
+  public static bool Yelling(string input)
+  {
+    return input.ToUpper() == input && Regex.IsMatch(input, "[a-zA-Z]+");
+  }
+
+  public static bool Asking(string input)
+  {
+    return input.Trim().EndsWith("?");
+  }
+}
