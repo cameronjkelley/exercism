3138405e3fb835b3de8d712943e4f167c0a047d3
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:18:49 -0400

    First Iteration

---
+++ csharp/raindrops/Raindrops.cs
@@ -0,0 +1,16 @@
+﻿public class Raindrops
+{
+  public static string Convert(int input)
+  {
+    string result = "";
+
+    if (input % 3 == 0)
+      result += "Pling";
+    if (input % 5 == 0)
+      result += "Plang";
+    if (input % 7 == 0)
+      result += "Plong";
+
+    return string.IsNullOrEmpty(result) ? input.ToString() : result;
+  }
+}
