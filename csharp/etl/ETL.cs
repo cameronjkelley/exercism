01c980c50c009ae064555de88f52daaeeb479692
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:01:41 -0400

    First Iteration

---
+++ csharp/etl/ETL.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+public class ETL
+{
+  public static Dictionary<string, int> Transform(Dictionary<int, IList<string>> input)
+  {
+    Dictionary<string, int> output = new Dictionary<string, int>();
+    foreach (int key in input.Keys)
+    {
+      IList<string> values = input[key];
+      foreach (string value in values)
+      {
+        output[value.ToLower()] = key;
+      }
+    }
+    return output;
+  }
+}
