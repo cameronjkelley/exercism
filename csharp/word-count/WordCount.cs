284662322e339fc5b5f190aa58d86ee60f55f2f4
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-15 10:29:34 -0400

    First Iteration

---
+++ csharp/word-count/WordCount.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class Phrase
+{
+  public static Dictionary<string, int> WordCount(string input)
+  {
+    Dictionary<string, int> output = new Dictionary<string, int>();
+    Match match = Regex.Match(input, @"\w+'\w+|\w+");
+
+    while (match.Success)
+    {
+      string word = match.Value.ToLower();
+      if (!output.ContainsKey(word))
+      {
+        output.Add(word, 0);
+      }
+      output[word]++;
+      match = match.NextMatch();
+    }
+    return output;
+  }
+}
