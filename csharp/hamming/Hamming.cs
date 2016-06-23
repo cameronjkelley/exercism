893ab607d0079b0092bf89079b6bb3c11bd6ffc7
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-23 09:48:14 -0400

    Second Iteration

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -3,15 +3,10 @@
   public static int Compute(string x, string y)
   {
     int diff = 0;
-
     for (int i = 0; i < x.Length; i++)
     {
-      if (x[i] != y[i])
-      {
-        diff++;
-      }
+      diff += x[i] != y[i] ? 1 : 0;
     }
-
     return diff;
   }
 }
