9a4a8709d3721f3231b836474aecd2399686dbd9
Cameron Kelley <i.am.cjk@gmail.com>
2016-06-23 09:50:36 -0400

    Second Iteration

--- csharp/hexadecimal/Hexadecimal.cs
+++ csharp/hexadecimal/Hexadecimal.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 
 public class Hexadecimal
 {
-  private static Dictionary<char, int> alphaNumerals = new Dictionary<char, int>
-  {
-    { 'a', 10 },
-    { 'b', 11 },
-    { 'c', 12 },
-    { 'd', 13 },
-    { 'e', 14 },
-    { 'f', 15 }
-  };
+  private static string Hex = "0123456789abcdef";
 
   public static int ToDecimal(string input)
   {
@@ -19,22 +10,13 @@ public class Hexadecimal
     int power = input.Length - 1;
     for (int i = 0; i < input.Length; i++)
     {
-      int number;
-      bool result = Int32.TryParse(input[i].ToString(), out number);
-      if (result)
+      if (Hex.IndexOf(input[i]) != -1)
       {
-        output += number * (int)Math.Pow(16, power);
+        output += Hex.IndexOf(input[i]) * (int)Math.Pow(16, power);
       }
       else
       {
-        if (alphaNumerals.ContainsKey(input[i]))
-        {
-          output += alphaNumerals[input[i]] * (int)Math.Pow(16, power);
-        }
-        else
-        {
-          return 0;
-        }
+        return 0;
       }
       power--;
     }
