2116e7e35f461d2171e0c89a7ecd2323cbc6eaf0
Cameron Kelley <i.am.cjk@gmail.com>
2016-07-08 10:58:36 -0400

    First Iteration

---
+++ csharp/simple-cipher/Cipher.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Cipher
+{
+  private static string alphabet = "abcdefghijklmnopqrstuvwxyz";
+  private static Random random = new Random();
+
+  public string Key { get; set; }
+
+  public Cipher()
+  {
+    Key = ProduceKey();
+  }
+
+  public Cipher(string key)
+  {
+    if (InvalidKey(key))
+      throw new ArgumentException();
+    Key = key;
+  }
+
+  public string Encode(string plainText)
+  {
+    return Translate(plainText, (x, y) => x + y);
+  }
+
+  public string Decode(string encodedText)
+  {
+    return Translate(encodedText, (x, y) => x - y);
+  }
+
+  private string Translate(string message, Func<int, int, int> createIndex)
+  {
+    string code = "";
+    for (int i = 0; i < message.Length; i++)
+    {
+      int newIndex = createIndex(alphabet.IndexOf(message[i]), alphabet.IndexOf(Key[i]));
+      if (newIndex >= 26)
+        code += alphabet[newIndex - 26];
+      else if (newIndex < 0)
+        code += alphabet[newIndex + 26];
+      else
+        code += alphabet[newIndex];
+    }
+    return code;
+  }
+
+  private static bool InvalidKey(string key)
+  {
+    return key == "" || Regex.IsMatch(key, "[^a-z]");
+  }
+
+  private string ProduceKey()
+  {
+    return new string(Enumerable.Repeat(alphabet, 100).Select(s => s[random.Next(s.Length)]).ToArray());
+  }
+}
