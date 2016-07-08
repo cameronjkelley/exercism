9974a0d3bf57e3c84b13f45536d671b5a6808159
Cameron Kelley <i.am.cjk@gmail.com>
2016-07-08 12:12:19 -0400

    Improved readability.

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -1,30 +1,29 @@
-﻿using System;
 using System.Text.RegularExpressions;
 
 public class PhoneNumber
 {
-  private string _number { get; set; }
+  private string number { get; set; }
 
   public PhoneNumber(string num)
   {
-    _number = FormatNumber(num);
+    number = FormatNumber(num);
   }
 
   public string Number()
   {
-    return _number;
+    return number;
   }
 
   public string AreaCode()
   {
-    return _number.Substring(0, 3);
+    return number.Substring(0, 3);
   }
 
   public override string ToString()
   {
-    string a = _number.Substring(0, 3),
-           b = _number.Substring(3, 3),
-           c = _number.Substring(6, 4);
+    string a = number.Substring(0, 3),
+           b = number.Substring(3, 3),
+           c = number.Substring(6, 4);
 
     return $"({a}) {b}-{c}";
   }
@@ -41,13 +40,10 @@ public class PhoneNumber
     }
 
     if (result.Length == 10)
-    {
       return result;
-    }
     else if (result.Length == 11 && result.IndexOf("1") == 0)
-    {
       return result.Substring(1);
-    }
-    return "0000000000";
+    else
+      return "0000000000";
   }
 }
