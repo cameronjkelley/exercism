2fa840369aca89091a9bacba68a2dc8ce8ee906f
Cameron Kelley <i.am.cjk@gmail.com>
2016-08-03 15:27:18 -0400

    First Iteration

---
+++ csharp/sieve/Sieve.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Sieve
+{
+	public static int[] Primes(int limit)
+	{
+		List<int> range = Enumerable.Range(2, limit - 1).ToList();
+		for (int i = 2; i < range.Count; i++)
+		{
+			for (int j = i; j < range.Count; j++)
+			{
+				range.Remove(i * j);
+			}
+		}
+		return range.ToArray();
+	}
+}
