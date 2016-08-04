827fe66566ae57cca4a203387b0491c3d197b793
Cameron Kelley <i.am.cjk@gmail.com>
2016-08-04 09:33:25 -0400

    Update Triangle.cs

--- csharp/triangle/Triangle.cs
+++ csharp/triangle/Triangle.cs
@@ -1,49 +1,37 @@
-﻿using System;
-
-public class Triangle
-{
-  public static TriangleKind Kind(decimal a, decimal b, decimal c)
-  {
-    if (NotATriangle(a,b,c))
-    {
-      throw new TriangleException();
-    }
-
-    if (IsEquilateral(a, b, c))
-    {
-      return TriangleKind.Equilateral;
-    }
-    else if (IsIsosceles(a, b, c))
-    {
-      return TriangleKind.Isosceles;
-    }
-    else
-    {
-      return TriangleKind.Scalene;
-    }
-  }
-
-  private static bool NotATriangle(decimal a, decimal b, decimal c)
-  {
-    return a <= 0 || b <= 0 || c <= 0 || a + b <= c || b + c <= a || a + c <= b;
-  }
-
-  private static bool IsEquilateral(decimal a, decimal b, decimal c)
-  {
-    return a == b && b == c;
-  }
-
-  private static bool IsIsosceles(decimal a, decimal b, decimal c)
-  {
-    return (a == b && a != c) || (a == c && b != c) || (b == c && b != a);
-  }
-}
+using System;
 
 public enum TriangleKind
 {
-  Equilateral,
-  Isosceles,
-  Scalene
+	Equilateral,
+	Isosceles,
+	Scalene
+}
+
+public class Triangle
+{
+	public static TriangleKind Kind(decimal a, decimal b, decimal c)
+	{
+		if (NotATriangle(a,b,c)) throw new TriangleException();
+
+		if (IsEquilateral(a, b, c)) return TriangleKind.Equilateral;
+		else if (IsIsosceles(a, b, c)) return TriangleKind.Isosceles;
+		else return TriangleKind.Scalene;
+	}
+
+	private static bool NotATriangle(decimal a, decimal b, decimal c)
+	{
+		return a <= 0 || b <= 0 || c <= 0 || a + b <= c || b + c <= a || a + c <= b;
+	}
+
+	private static bool IsEquilateral(decimal a, decimal b, decimal c)
+	{
+		return a == b && b == c;
+	}
+
+	private static bool IsIsosceles(decimal a, decimal b, decimal c)
+	{
+		return (a == b && a != c) || (a == c && b != c) || (b == c && b != a);
+	}
 }
 
 public class TriangleException : Exception { }
