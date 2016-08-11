1dbce9f0c3b1ba2b345c5097c12dbc2476e64964
Cameron Kelley <i.am.cjk@gmail.com>
2016-08-11 10:27:23 -0400

    Update Strain.cs

--- csharp/strain/Strain.cs
+++ csharp/strain/Strain.cs
@@ -18,7 +18,8 @@ public static class Strain
     List<T> result = new List<T>();
     foreach (T item in source)
     {
-      if (predicate(item)) result.Add(item);
+      if (predicate(item)) 
+        result.Add(item);
     }
     return result;
   }
