4d4dd685b62863f18705e6bb5cbc135b32061cec
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-06 11:32:10 -0400

    First iteration

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,12 @@
+﻿var words = function (input) {
+	this.count = function (input) {
+		var output = {};
+		var array = input.trim().toLowerCase().split(/\s+/);
+		array.forEach(function (word) {
+			output.hasOwnProperty(word) ? output[word]++ : output[word] = 1;
+		});
+		return output;
+	}
+};
+
+module.exports = words;
