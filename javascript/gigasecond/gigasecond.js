86190b21330262f255803bea3d3554de0f2cd547
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-06 11:31:17 -0400

    First iteration

---
+++ javascript/gigasecond/gigasecond.js
@@ -0,0 +1,10 @@
+﻿var Gigasecond = function (date) {
+	this.startDate = date;
+};
+
+Gigasecond.prototype.date = function () {
+	var date = this.startDate.getTime();
+	return new Date(date + 1000000000000);
+};
+
+module.exports = Gigasecond;
