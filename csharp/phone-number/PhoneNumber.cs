09f8c3a1353855cc313c5dff8ce38827b1900849
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-06 13:54:03 -0400

    Update PhoneNumber.cs

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -2,48 +2,39 @@ using System.Text.RegularExpressions;
 
 public class PhoneNumber
 {
-  private string number { get; set; }
+	private string number { get; set; }
 
-  public PhoneNumber(string num)
-  {
-    number = FormatNumber(num);
-  }
+	public PhoneNumber(string num)
+	{
+		number = FormatNumber(num);
+	}
 
-  public string Number()
-  {
-    return number;
-  }
+	public string Number()
+	{
+		return number;
+	}
 
-  public string AreaCode()
-  {
-    return number.Substring(0, 3);
-  }
+	public string AreaCode()
+	{
+		return number.Substring(0, 3);
+	}
 
-  public override string ToString()
-  {
-    string a = number.Substring(0, 3),
-           b = number.Substring(3, 3),
-           c = number.Substring(6, 4);
+	public override string ToString()
+	{
+		string a = number.Substring(0, 3),
+					 b = number.Substring(3, 3),
+					 c = number.Substring(6, 4);
 
-    return $"({a}) {b}-{c}";
-  }
+		return $"({a}) {b}-{c}";
+	}
 
-  private string FormatNumber(string num)
-  {
-    string result = "";
-    Regex pattern = new Regex(@"\d+");
-    MatchCollection matches = pattern.Matches(num);
+	private string FormatNumber(string num)
+	{
+		Regex pattern = new Regex(@"\W");
+		num = pattern.Replace(num, "");
 
-    foreach (Match match in matches)
-    {
-      result += match.Value;
-    }
-
-    if (result.Length == 10)
-      return result;
-    else if (result.Length == 11 && result.IndexOf("1") == 0)
-      return result.Substring(1);
-    else
-      return "0000000000";
-  }
+		if (num.Length == 10) return num;
+		else if (num.Length == 11 && num.StartsWith("1")) return num.Substring(1);
+		else return "0000000000";
+	}
 }
