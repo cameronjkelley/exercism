4cf5074188c2bf65aa3050888825acf3e3ecbfb8
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-14 13:30:30 -0400

    Update hamming.js

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,17 +1,10 @@
-var compute = function(x, y) {
-	
-	var result = 0;
-	
-	if (x.length != y.length) {
+function compute(x, y) {
+	if (x.length != y.length)
 		throw ('DNA strands must be of equal length.');
-	} else {
-		for (var i = 0; i < x.length; i++) {
-			if (x[i] != y[i]) {
-				result++;
-			}
-		}
-	}
-	return result;
+	else
+		return x.split("").reduce(function(p, _c, i) {
+			return x[i] !== y[i] ? p + 1 : p + 0;
+		}, 0);
 };
 
 module.exports = compute;
