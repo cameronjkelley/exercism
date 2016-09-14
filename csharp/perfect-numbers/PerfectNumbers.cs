c4e7a1ed39f401b29e8c67399e04736ed5c70c7d
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-14 12:52:07 -0400

    Update PerfectNumbers.cs

--- csharp/perfect-numbers/PerfectNumbers.cs
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -1,18 +1,18 @@
-﻿using System.Linq;
+using System.Linq;
 
 class PerfectNumbers
 {
+	public enum NumberType
+	{
+		Deficient, Abundant, Perfect
+	}
+
 	public static NumberType Classify(int number)
 	{
-		int sum = Enumerable.Range(1, number - 1).Where(x => number % x == 0).Sum();
+		int sum = Enumerable.Range(1, number / 2).Where(x => number % x == 0).Sum();
 
 		if (sum < number) return NumberType.Deficient;
 		else if (sum > number) return NumberType.Abundant;
 		else return NumberType.Perfect;
 	}
-}
-
-public enum  NumberType
-{
-	Deficient, Abundant, Perfect
 }
