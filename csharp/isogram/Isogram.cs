1903e6058422ef17b5f893ed69947833b096269a
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-14 11:30:23 -0400

    First iteration

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+class Isogram
+{
+	public static bool IsIsogram(string input)
+	{
+		string lowered = input.ToLower();
+		return input.Length == lowered.Where(x => lowered.IndexOf(x) == lowered.LastIndexOf(x) || (x == ' ' || x == '-')).Count();
+	}
+}
