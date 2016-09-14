583d805aeb754e83a917a01def7dc432f7a881f1
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-14 11:48:45 -0400

    Update LargestSeriesProduct.cs

--- csharp/largest-series-product/LargestSeriesProduct.cs
+++ csharp/largest-series-product/LargestSeriesProduct.cs
@@ -5,30 +5,24 @@ using System.Linq;
 public class LargestSeriesProduct
 {
 	private List<int> digits = new List<int>();
-
 	public LargestSeriesProduct (string digits)
 	{
 		this.digits = digits.Select(x => int.Parse(x.ToString())).ToList();
 	}
-
 	public int GetLargestProduct(int length) 
 	{
-		if (length > digits.Count) {
-			throw new ArgumentException ();
-		}
-		if (length == 0) {
-			return 1;
-		}
+		if (length > digits.Count) throw new ArgumentException();
+		if (length == 0) return 1;
 
 		int largestProduct = 0;
-		for (int i = 0; i <= digits.Count - length; i++) {
+		for (int i = 0; i <= digits.Count - length; i++)
+		{
 			int product = digits[i];
-			for (int j = 1; j < length; j++) {
+			for (int j = 1; j < length; j++)
+			{
 				product *= digits [i + j]; 
 			}
-			if (product > largestProduct) {
-				largestProduct = product;
-			}
+			if (product > largestProduct) largestProduct = product;
 		}
 		return largestProduct;
 	}
