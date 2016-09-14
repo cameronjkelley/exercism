828aadf7e012f3fadfee53df00f676bdf2cfbaf1
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-14 10:13:40 -0400

    First iteration

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+class PerfectNumbers
+{
+	public static NumberType Classify(int number)
+	{
+		int sum = Enumerable.Range(1, number - 1).Where(x => number % x == 0).Sum();
+
+		if (sum < number) return NumberType.Deficient;
+		else if (sum > number) return NumberType.Abundant;
+		else return NumberType.Perfect;
+	}
+}
+
+public enum  NumberType
+{
+	Deficient, Abundant, Perfect
+}
