39146a2896cec403edaff2a7463a9a538c599957
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-14 12:22:24 -0400

    Update SumOfMultiples.cs

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -2,5 +2,5 @@ using System.Linq;
 
 public class SumOfMultiples
 {
-  return return Enumerable.Range(1, limit - 1).Where(x => multiples.Any(y => x % y == 0)).Sum();
+  return Enumerable.Range(1, limit - 1).Where(x => multiples.Any(y => x % y == 0)).Sum();
 }
