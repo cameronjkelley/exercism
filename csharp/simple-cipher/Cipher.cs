779a1a55368b1dcce583ea6e5ec754b07bb688d4
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-14 12:56:36 -0400

    Update Cipher.cs

--- csharp/simple-cipher/Cipher.cs
+++ csharp/simple-cipher/Cipher.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -28,29 +28,29 @@ public class Cipher
   public string Decode(string encodedText)
   {
     return Translate(encodedText, (x, y) => x - y);
-  }
+	}
 
-  private string Translate(string message, Func<int, int, int> createIndex)
+	private string ProduceKey()
+	{
+		return new string(Enumerable.Repeat(alphabet, 100).Select(s => s[random.Next(s.Length)]).ToArray());
+	}
+
+	private static bool InvalidKey(string key)
+	{
+		return key == "" || Regex.IsMatch(key, "[^a-z]");
+	}
+
+	private string Translate(string message, Func<int, int, int> createIndex)
   {
-    string code = "";
+    string newMessage = "";
     for (int i = 0; i < message.Length; i++)
     {
       int newIndex = createIndex(alphabet.IndexOf(message[i]), alphabet.IndexOf(Key[i]));
-      
-      if (newIndex >= 26) code += alphabet[newIndex - 26];
-      else if (newIndex < 0) code += alphabet[newIndex + 26];
-      else code += alphabet[newIndex];
+
+      if (newIndex >= 26) newMessage += alphabet[newIndex - 26];
+      else if (newIndex < 0) newMessage += alphabet[newIndex + 26];
+      else newMessage += alphabet[newIndex];
     }
-    return code;
-  }
-
-  private static bool InvalidKey(string key)
-  {
-    return key == "" || Regex.IsMatch(key, "[^a-z]");
-  }
-
-  private string ProduceKey()
-  {
-    return new string(Enumerable.Repeat(alphabet, 100).Select(s => s[random.Next(s.Length)]).ToArray());
+    return newMessage;
   }
 }
