3b489dab66a5caf59342b4ef8ba0b1b7ed2bdc89
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-14 12:21:51 -0400

    Update SumOfMultiples.cs

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -3,21 +3,5 @@ using System.Linq;
 
 public class SumOfMultiples
 {
-  public static int To(int[] multiples, int limit)
-  {
-    List<int> result = new List<int>();
-    IEnumerable<int> range = Enumerable.Range(1, limit - 1);
-
-    foreach (int x in range)
-    {
-      foreach (int y in multiples)
-      {
-        if (x % y == 0 && !result.Contains(x))
-        {
-          result.Add(x);
-        }
-      }
-    }
-    return result.Sum();
-  }
+  return return Enumerable.Range(1, limit - 1).Where(x => multiples.Any(y => x % y == 0)).Sum();
 }
