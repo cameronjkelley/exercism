4e2dd7778a5ae6b73e5713006369f3c2c32e7d8b
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-16 14:28:18 -0400

    First iteration

---
+++ javascript/atbash-cipher/atbash-cipher.js
@@ -0,0 +1,11 @@
+var letters = "abcdefghijklmnopqrstuvwxyz";
+
+var atbash = {
+  encode: function(text) {
+    return text.toLowerCase().replace(/\W/g, "").split("").map(
+    (char) => letters.indexOf(char) === -1 ? char : letters[25 - letters.indexOf(char)])
+    .join("").match(/.{1,5}/g).join(" ");
+  }
+};
+
+module.exports = atbash;
