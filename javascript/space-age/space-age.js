38dd2cfd14a432a16b6fcf51d28293f651532ba2
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-16 20:58:01 -0400

    Update space-age.js

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -6,13 +6,13 @@ function SpaceAge(seconds) {
   this.seconds = seconds;
   this.earthPeriod = seconds / 31557600;
   this.orbitalPeriods = {
-    "Mercury": 0.2408467,
-    "Venus": 0.61519726,
-    "Mars": 1.8808158,
-    "Jupiter": 11.862615,
-    "Saturn": 29.447498,
-    "Uranus": 84.016846,
-    "Neptune":  164.79132
+    Mercury: 0.2408467,
+    Venus: 0.61519726,
+    Mars: 1.8808158,
+    Jupiter: 11.862615,
+    Saturn: 29.447498,
+    Uranus: 84.016846,
+    Neptune:  164.79132
   };
   this.onEarth = function() {
     return parseTime(this.earthPeriod);
