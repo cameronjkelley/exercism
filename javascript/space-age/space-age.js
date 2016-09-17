cd38d6810cd8e56babffd24c32a9b47e2a6f0552
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-16 21:09:05 -0400

    Update space-age.js

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -5,38 +5,29 @@ function parseTime(time) {
 function SpaceAge(seconds) {
   this.seconds = seconds;
   this.earthPeriod = seconds / 31557600;
-  this.orbitalPeriods = {
-    Mercury: 0.2408467,
-    Venus: 0.61519726,
-    Mars: 1.8808158,
-    Jupiter: 11.862615,
-    Saturn: 29.447498,
-    Uranus: 84.016846,
-    Neptune:  164.79132
-  };
   this.onEarth = function() {
     return parseTime(this.earthPeriod);
   };
   this.onMercury = function() {
-    return parseTime(this.earthPeriod / this.orbitalPeriods["Mercury"]);
+    return parseTime(this.earthPeriod / 0.2408467;
   };
   this.onVenus = function() {
-    return parseTime(this.earthPeriod / this.orbitalPeriods["Venus"]);
+    return parseTime(this.earthPeriod / 0.61519726;
   };
   this.onMars = function() {
-    return parseTime(this.earthPeriod / this.orbitalPeriods["Mars"]);
+    return parseTime(this.earthPeriod / 1.8808158;
   };
   this.onJupiter = function() {
-    return parseTime(this.earthPeriod / this.orbitalPeriods["Jupiter"]);
+    return parseTime(this.earthPeriod / 11.862615;
   };
   this.onSaturn = function() {
-    return parseTime(this.earthPeriod / this.orbitalPeriods["Saturn"]);
+    return parseTime(this.earthPeriod / 29.447498;
   };
   this.onUranus = function() {
-    return parseTime(this.earthPeriod / this.orbitalPeriods["Uranus"]);
+    return parseTime(this.earthPeriod / 84.016846;
   };
   this.onNeptune = function() {
-    return parseTime(this.earthPeriod / this.orbitalPeriods["Neptune"]);
+    return parseTime(this.earthPeriod / 164.79132;
   };
 };
 
