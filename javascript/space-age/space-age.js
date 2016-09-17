ff42a249bd4bae67070eb7eb8fc0fca198ab741a
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-16 21:09:46 -0400

    Update space-age.js

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -9,25 +9,25 @@ function SpaceAge(seconds) {
     return parseTime(this.earthPeriod);
   };
   this.onMercury = function() {
-    return parseTime(this.earthPeriod / 0.2408467;
+    return parseTime(this.earthPeriod / 0.2408467);
   };
   this.onVenus = function() {
-    return parseTime(this.earthPeriod / 0.61519726;
+    return parseTime(this.earthPeriod / 0.61519726);
   };
   this.onMars = function() {
-    return parseTime(this.earthPeriod / 1.8808158;
+    return parseTime(this.earthPeriod / 1.8808158);
   };
   this.onJupiter = function() {
-    return parseTime(this.earthPeriod / 11.862615;
+    return parseTime(this.earthPeriod / 11.862615);
   };
   this.onSaturn = function() {
-    return parseTime(this.earthPeriod / 29.447498;
+    return parseTime(this.earthPeriod / 29.447498);
   };
   this.onUranus = function() {
-    return parseTime(this.earthPeriod / 84.016846;
+    return parseTime(this.earthPeriod / 84.016846);
   };
   this.onNeptune = function() {
-    return parseTime(this.earthPeriod / 164.79132;
+    return parseTime(this.earthPeriod / 164.79132);
   };
 };
 
