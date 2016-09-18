689782bf1a8ff7a2bd0f0b567078584d15c229af
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-18 12:50:30 -0400

    Update anagram.js

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -2,10 +2,9 @@ function Anagram(input) {
   this.sorted = input.toLowerCase().split("").sort().join("");
   this.matches = function(words) {
     words = Array.isArray(words) ? words : Array.from(arguments);
-    return words.filter(function(word) {
-       return word.toLowerCase() !== input.toLowerCase() &&
-              new Anagram(word).sorted === new Anagram(input).sorted;
-    });
+    return words.filter(word => 
+      word.toLowerCase() !== input.toLowerCase() 
+      && new Anagram(word).sorted === new Anagram(input).sorted);
   };
 };
 
