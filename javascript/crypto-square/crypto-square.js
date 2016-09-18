fda39afd8494f959f4029ff75ad479a0cd889849
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-18 16:35:51 -0400

    Update crypto-square.js

--- javascript/crypto-square/crypto-square.js
+++ javascript/crypto-square/crypto-square.js
@@ -8,7 +8,17 @@ function Crypto(text) {
   this.plaintextSegments = function() {
     return this.normalizePlaintext().match(new RegExp(".{1," + this.size() + "}", "g"));
   };
-  // this.ciphertext = function() {};
+  this.ciphertext = function() {
+    var text = this.plaintextSegments();
+    var output = "";
+    for (var i = 0; i < text[0].length; i++) {
+      for (var j = 0; j < text.length; j++) {
+        if (i < text[j].length)
+          output += text[j][i];
+      }
+    }
+    return output;
+  };
 };
 
 module.exports = Crypto;
