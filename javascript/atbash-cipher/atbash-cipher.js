58c764b738bd3ba7bc1514208ee20a797612366d
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-18 16:30:51 -0400

    Update atbash-cipher.js

--- javascript/atbash-cipher/atbash-cipher.js
+++ javascript/atbash-cipher/atbash-cipher.js
@@ -2,8 +2,8 @@ var letters = "abcdefghijklmnopqrstuvwxyz";
 
 var atbash = {
   encode: function(text) {
-    return text.toLowerCase().replace(/\W/g, "").split("").map(
-    (char) => letters.indexOf(char) === -1 ? char : letters[25 - letters.indexOf(char)])
+    return text.toLowerCase().replace(/\W/g, "").split("").map(char => 
+    letters.indexOf(char) === -1 ? char : letters[25 - letters.indexOf(char)])
     .join("").match(/.{1,5}/g).join(" ");
   }
 };
