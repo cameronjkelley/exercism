bdb6daaf04127e84748c4433428eee3bc5f02c6a
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-18 13:22:19 -0400

    Update trinary.js

--- javascript/trinary/trinary.js
+++ javascript/trinary/trinary.js
@@ -1,6 +1,7 @@
 function Trinary(num) {
   this.toDecimal = function() {
-    return num.match(/[^012]/g) ? 0 : num.split("").reverse().reduce((p, e, i) => p + e * Math.pow(3, i), 0);  };
+    return num.match(/[^012]/g) ? 0 : num.split("").reverse().reduce((p, e, i) => p + e * Math.pow(3, i), 0);
+  };
 };
 
 module.exports = Trinary;
