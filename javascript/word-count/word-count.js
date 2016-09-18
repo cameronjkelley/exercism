9e6a6d3267d4a2e9f77890e0a233028655d348dc
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-17 20:47:32 -0400

    Update word-count.js

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,10 +1,7 @@
-﻿var words = function (input) {
-	this.count = function (input) {
+﻿function words(input) {
+	this.count = function(input) {
 		var output = {};
-		var array = input.trim().toLowerCase().split(/\s+/);
-		array.forEach(function (word) {
-			output.hasOwnProperty(word) ? output[word]++ : output[word] = 1;
-		});
+		input.trim().toLowerCase().split(/\s+/).forEach((word) => output.hasOwnProperty(word) ? output[word]++ : output[word] = 1);
 		return output;
 	}
 };
