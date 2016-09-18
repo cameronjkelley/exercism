8eab90fc0af245c7a831ae0965f8445f4625c23c
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-18 17:19:05 -0400

    Update Cipher.cs

--- csharp/simple-cipher/Cipher.cs
+++ csharp/simple-cipher/Cipher.cs
@@ -34,22 +34,18 @@ public class Cipher
 	{
 		return new string(Enumerable.Repeat(alphabet, 100).Select(s => s[random.Next(s.Length)]).ToArray());
 	}
-
+	
 	private static bool InvalidKey(string key)
 	{
 		return key == "" || Regex.IsMatch(key, "[^a-z]");
 	}
-
+	
 	private string Translate(string message, Func<int, int, int> createIndex)
   {
     string newMessage = "";
     for (int i = 0; i < message.Length; i++)
     {
-      int newIndex = createIndex(alphabet.IndexOf(message[i]), alphabet.IndexOf(Key[i]));
-
-      if (newIndex >= 26) newMessage += alphabet[newIndex - 26];
-      else if (newIndex < 0) newMessage += alphabet[newIndex + 26];
-      else newMessage += alphabet[newIndex];
+      newMessage += letters[(createIndex(letters.IndexOf(message[i]), letters.IndexOf(Key[i])) % 26)];
     }
     return newMessage;
   }
