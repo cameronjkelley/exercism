6378ed24346e23e1e4eeb009ecb677ef2133d419
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-19 11:41:57 -0400

    Add files via upload

---
+++ csharp/proverb/Proverb.cs
@@ -0,0 +1,20 @@
+﻿class Proverb
+{
+	private static string[] proverb = {
+		"For want of a nail the shoe was lost.\n",
+		"For want of a shoe the horse was lost.\n",
+		"For want of a horse the rider was lost.\n",
+		"For want of a rider the message was lost.\n",
+		"For want of a message the battle was lost.\n",
+		"For want of a battle the kingdom was lost.\n",
+		"And all for the want of a horseshoe nail."
+	};
+	public static string Line(int num)
+	{
+		return proverb[num - 1].Trim('\n');
+	}
+	public static string AllLines()
+	{
+		return string.Join("", proverb);
+	}
+}
