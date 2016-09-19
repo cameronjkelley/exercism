ed2329cfc2402007cf56604f267888f738e338c2
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-19 11:21:52 -0400

    First iteration

---
+++ csharp/bank-account/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class BankAccount
+{
+	private enum Status { Open, Closed };
+	private Status Account;
+	private int Balance;
+	public void Open()
+	{
+		Account = Status.Open;
+		Balance = 0;
+	}
+	public int GetBalance()
+	{
+		if (Account == Status.Open) return Balance;
+		else throw new InvalidOperationException();
+	}
+	public void UpdateBalance(int amount)
+	{
+		if (Account == Status.Open) Balance += amount;
+		else throw new InvalidOperationException();
+	}
+	public void Close()
+	{
+		Account = Status.Closed;
+	}
+}
