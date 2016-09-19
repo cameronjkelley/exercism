0443de2aee57e986ab52bda2bcb1d45f5372d175
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-19 12:57:14 -0400

    Update allergies.js

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -1,18 +1,19 @@
+var allergenScores = {
+  "cats": 128,
+  "pollen": 64,
+  "chocolate": 32,
+  "tomatoes": 16,
+  "strawberries": 8,
+  "shellfish": 4,
+  "peanuts": 2,
+  "eggs": 1
+};
+
 function Allergies(score) {
-  var allergenScores = {
-    "eggs": 1,
-    "peanuts": 2,
-    "shellfish": 4,
-    "strawberries": 8,
-    "tomatoes": 16,
-    "chocolate": 32,
-    "pollen": 64,
-    "cats": 128
-  };
   this.list = function() {
     var list = [];
-    Object.keys(allergenScores).reverse().forEach(function(key) {
-      if (score > 255) score -= 256;
+    Object.keys(allergenScores).forEach(function(key) {
+      score %= 256;
       if (score > 0 && score / allergenScores[key] >= 1) {
         list.unshift(key);
         score -= allergenScores[key];
