0241c1bb9dcab36d923755e12c311ca27d65f582
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-20 17:43:58 -0400

    Update Queens.cs

--- csharp/queen-attack/Queens.cs
+++ csharp/queen-attack/Queens.cs
@@ -4,7 +4,6 @@ public class Queen
 {
 	public int Rank { get; private set; }
 	public int File { get; private set; }
-
 	public Queen(int x, int y)
 	{
 		Rank = x;
@@ -16,18 +15,16 @@ public class Queens
 {
 	private static Queen White { get; set; }
 	private static Queen Black { get; set; }
-
 	public Queens(Queen white, Queen black)
 	{
 		White = white;
 		Black = black;
 
-		if (White.Rank == Black.Rank && White.File == Black.File)
-			throw new ArgumentException();
+		if (White.Rank == Black.Rank && White.File == Black.File) throw new ArgumentException();
 	}
-
 	public bool CanAttack()
 	{
-		return White.Rank == Black.Rank || White.File == Black.File || Math.Abs(Black.Rank - White.Rank) == Math.Abs(Black.File - White.File);
+		return White.Rank == Black.Rank ||  White.File == Black.File || 
+		Math.Abs(White.Rank - Black.Rank) == Math.Abs(White.File - Black.File);
 	}
 }
