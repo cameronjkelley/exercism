1c98ac3a3b3a4dabf361b50b55d999a485c942ee
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-20 15:47:19 -0400

    First iteration

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Grains
+{
+	public static ulong Square(int num)
+	{
+		return (ulong)Math.Pow(2, num - 1);
+	}
+	public static ulong Total()
+	{
+		return Square(65) - 1;
+	}
+}
