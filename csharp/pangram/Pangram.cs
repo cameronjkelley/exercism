160859b47c9eb123fe7f4dc285bd510637c1af68
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-20 16:43:13 -0400

    1st Iteration

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+
+class Pangram
+{
+	public static bool IsPangram(string phrase)
+	{
+		phrase = Regex.Replace(phrase.ToLower(), @"\W|\d|_|[^\u0000-\u007F]+", "");
+		return phrase.Where((x, i) => phrase.IndexOf(x) == i).Count() == 26;
+	}
+}
