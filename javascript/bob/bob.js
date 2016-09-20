91b0ca07e40a39bb7e3cd423c7ea17ada854f928
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-20 09:28:43 -0400

    Update bob.js

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,12 +1,3 @@
-function Bob() {
-	this.hey = function(input) {
-		if (yelling(input)) return "Whoa, chill out!";
-		else if (asking(input)) return "Sure.";
-		else if (telling(input)) return "Whatever.";
-		else return "Fine. Be that way!";
-	};
-};
-
 function asking(input) {
 	return input.slice(-1) === "?";
 };
@@ -17,6 +8,15 @@ function telling(input) {
 
 function yelling(input) {
 	return input.toUpperCase() === input && input.toLowerCase() !== input;
+};
+
+function Bob() {
+	this.hey = function(input) {
+		if (yelling(input)) return "Whoa, chill out!";
+		else if (asking(input)) return "Sure.";
+		else if (telling(input)) return "Whatever.";
+		else return "Fine. Be that way!";
+	};
 };
 
 module.exports = Bob;
