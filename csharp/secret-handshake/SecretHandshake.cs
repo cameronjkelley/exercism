59c0944e0241358775d26f196b8550824039363f
Cameron Kelley <i.am.cjk@gmail.com>
2016-09-21 12:23:36 -0400

    Update SecretHandshake.cs

--- csharp/secret-handshake/SecretHandshake.cs
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -15,13 +15,9 @@ public class SecretHandshake
     List<string> result = new List<string>();
     foreach (KeyValuePair<string, string> command in commands) {
       int key = Convert.ToInt32(command.Key, 2);
-      if ((x & key) == key)
-        result.Add(command.Value);
+      if ((x & key) == key) result.Add(command.Value);
     }
-
-    if (x > (1 << commands.Count))
-      result.Reverse();
-
+    if (x > (1 << commands.Count)) result.Reverse();
     return result.ToArray();
   }
 }
