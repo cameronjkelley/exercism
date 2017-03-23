22dbcefacf5599776983e3a8ca4190090e8e8694
Cameron Kelley <i.am.cjk@gmail.com>
2017-03-23 15:57:20 -0400

    Update ScaleGenerator.cs

--- csharp/scale-generator/ScaleGenerator.cs
+++ csharp/scale-generator/ScaleGenerator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,12 +6,15 @@ class ScaleGenerator
 {
 	private static string[] Flats = { "F", "Ab", "Bb", "Db", "Eb", "Gb", "c", "d", "f", "g", "bb", "eb" };
 	private static string[] Notes = { "Ab", "A", "A#", "Bb", "B", "C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#" };
+	
 	public static IEnumerable<string> Pitches(string key, string intervals)
 	{
-		List<string> scale = new List<string> { };
 		string[] notes = Flats.Contains(key) ? Notes.Where(x => !x.Contains("#")).ToArray() : Notes.Where(x => !x.Contains("b")).ToArray();
 		key = key.Length == 2 ? key[0].ToString().ToUpper() + key[1] : key.ToUpper();
 		int idx = Array.IndexOf(notes, key);
+		
+		List<string> scale = new List<string>();
+		
 		for (int i = 1; i <= intervals.Length; i++)
 		{
 			if (idx > notes.Length - 1) idx -= notes.Length;
