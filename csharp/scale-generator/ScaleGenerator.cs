f70ab930f384c2d116706f59cc9fe819ce11dca5
Cameron Kelley <i.am.cjk@gmail.com>
2017-04-19 21:51:48 -0400

    Update ScaleGenerator.cs

--- csharp/scale-generator/ScaleGenerator.cs
+++ csharp/scale-generator/ScaleGenerator.cs
@@ -9,21 +9,29 @@ class ScaleGenerator
 	
 	public static IEnumerable<string> Pitches(string key, string intervals)
 	{
-		string[] notes = Flats.Contains(key) ? Notes.Where(x => !x.Contains("#")).ToArray() : Notes.Where(x => !x.Contains("b")).ToArray();
-		key = key.Length == 2 ? key[0].ToString().ToUpper() + key[1] : key.ToUpper();
+		string[] notes = Flats.Contains(key) ? 
+			Notes.Where(x => !x.Contains("#")).ToArray() : 
+			Notes.Where(x => !x.Contains("b")).ToArray();
+		key = key.Length == 2 ? 
+			key[0].ToString().ToUpper() + key[1] : 
+			key.ToUpper();
 		int idx = Array.IndexOf(notes, key);
 		
 		List<string> scale = new List<string>();
 		
 		for (int i = 1; i <= intervals.Length; i++)
 		{
-			if (idx > notes.Length - 1) idx -= notes.Length;
+			if (idx > notes.Length - 1) 
+				idx -= notes.Length;
 			
 			scale.Add(notes[idx]);
 			
-			if (intervals[i - 1] == 'm') idx += 1;
-			else if (intervals[i - 1] == 'M') idx += 2;
-			else idx += 3;
+			if (intervals[i - 1] == 'm') 
+				idx += 1;
+			else if (intervals[i - 1] == 'M') 
+				idx += 2;
+			else 
+				idx += 3;
 		}
 		return scale.ToArray();
 	}
