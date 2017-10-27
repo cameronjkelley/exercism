7e46cec8babb18570c27b7629e74d45b2588f447
Cameron Kelley <i.am.cjk@gmail.com>
2017-10-27 13:00:26 -0400

    Readability

--- csharp/pascals-triangle/PascalsTriangle.cs
+++ csharp/pascals-triangle/PascalsTriangle.cs
@@ -6,17 +6,24 @@ public class PascalsTriangle
   {
     List<List<int>> rows = new List<List<int>>();
     rows.Add(new List<int>() { 1 });
+    
     for (int i = 1; i < number; i++)
     {
       List<int> row = new List<int>();
       row.Add(1);
+      
       for (int j = 1; j < i; j++)
       {
-        if (i > 1) row.Add(rows[i - 1][j - 1] + rows[i - 1][j]);
+        if (i > 1) 
+        {
+          row.Add(rows[i - 1][j - 1] + rows[i - 1][j]);
+        }
       }
+      
       row.Add(1);
       rows.Add(row);
     }
+   
     return rows;
   }
 }
