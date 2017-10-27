03122b2a2db06bac658fdedf8d334a28c8c42e24
Cameron Kelley <i.am.cjk@gmail.com>
2017-10-27 12:51:17 -0400

    Readability

--- csharp/hexadecimal/Hexadecimal.cs
+++ csharp/hexadecimal/Hexadecimal.cs
@@ -9,10 +9,18 @@ public class Hexadecimal
     int power = input.Length - 1;
     for (int i = 0; i < input.Length; i++)
     {
-      if (Hex.IndexOf(input[i]) == -1) return 0;
-      else output += Hex.IndexOf(input[i]) * (int)Math.Pow(16, power);
+      if (Hex.IndexOf(input[i]) == -1) 
+      {
+        return 0;
+      }
+      else 
+      {
+        output += Hex.IndexOf(input[i]) * (int)Math.Pow(16, power);
+      }
+      
       power--;
     }
+    
     return output;
   }
 }
