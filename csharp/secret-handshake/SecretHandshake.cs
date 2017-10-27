27d461217556a0130e9bdc1a463463ac6b9f5e92
Cameron Kelley <i.am.cjk@gmail.com>
2017-10-27 13:27:36 -0400

    Readability

--- csharp/secret-handshake/SecretHandshake.cs
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 
 public class SecretHandshake
 {
-  private static Dictionary<string, string> commands = new Dictionary<string, string> {
+  private static Dictionary<string, string> commands = new Dictionary<string, string> 
+  {
     { "1", "wink"},
     { "10", "double blink" },
     { "100", "close your eyes" },
@@ -13,11 +14,21 @@ public class SecretHandshake
   public static string[] Commands (int x)
   {
     List<string> result = new List<string>();
-    foreach (KeyValuePair<string, string> command in commands) {
+    
+    foreach (KeyValuePair<string, string> command in commands) 
+    {
       int key = Convert.ToInt32(command.Key, 2);
-      if ((x & key) == key) result.Add(command.Value);
+      if ((x & key) == key) 
+      {  
+        result.Add(command.Value);
+      }
     }
-    if (x > (1 << commands.Count)) result.Reverse();
+    
+    if (x > (1 << commands.Count)) 
+    {
+      result.Reverse();
+    }
+    
     return result.ToArray();
   }
 }
