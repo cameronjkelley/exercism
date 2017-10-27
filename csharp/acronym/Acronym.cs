4d06ee53dc22f3654419b239a9a0a60122c771ab
Cameron Kelley <i.am.cjk@gmail.com>
2017-10-27 11:41:58 -0400

    Readability

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -1,18 +1,23 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
 public class Acronym
 {
   public static string Abbreviate(string input)
   {
-    if (string.IsNullOrEmpty(input)) return string.Empty;
-
-    string output = "";
+    if (string.IsNullOrEmpty(input)) 
+    {
+      return string.Empty;
+    }
 
     Regex pattern = new Regex(@"\b[A-Za-z]|[A-Z](?=[a-z])");
     MatchCollection matches = pattern.Matches(input);
-
+    string output = "";
+    
     foreach (Match match in matches)
+    {
       output += match.Value;
+    }
+    
     return output.ToUpper();
   }
 }
