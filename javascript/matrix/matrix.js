dea5fed6da0b192da9ace07d64e648af6bbb41a0
Cameron Kelley <i.am.cjk@gmail.com>
2018-04-04 14:49:00 -0400

    First iteration

---
+++ javascript/matrix/matrix.js
@@ -0,0 +1,12 @@
+function Matrix (input) {
+	this.rows = 
+		input.split("\n")
+			.map(row => row.split(" ")
+				.map(x => parseInt(x)));
+	this.columns = 
+		this.rows[0]
+			.map((col, i) => this.rows
+				.map((row) => row[i]));
+};
+
+module.exports = Matrix;
