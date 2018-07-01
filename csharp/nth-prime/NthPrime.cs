987888ac814ee14bc064cf884455e8338b5d9d0e
Cameron Kelley <i.am.cjk@gmail.com>
2018-07-01 10:07:19 -0400

    Removed unnecessary conditional

--- csharp/nth-prime/NthPrime.cs
+++ csharp/nth-prime/NthPrime.cs
@@ -9,22 +9,17 @@ public class NthPrime
     int p = 3;
     while (primes.Count != x)
     {
+      p += 2;
       if (isPrime(p))
       {
         primes.Add(p);
       }
-      p += 2;
     }
     return primes[x - 1];
   }
 
   private static bool isPrime(int x)
   {
-    if (x % 2 == 0)
-    {
-      return false;
-    }
-    
     for(int i = 3; i <= Math.Sqrt(x); i += 2)
     {
       if (x % i == 0)
