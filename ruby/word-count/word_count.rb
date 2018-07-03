0e77532362ec970a6c73201232fb36c14ccd5c9d
Cameron Kelley <i.am.cjk@gmail.com>
2018-07-03 16:43:48 -0400

    Update word_count.rb

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,12 +1,14 @@
 class Phrase
-  attr_reader :word_count
-
   def initialize(phrase)
-    @word_count = Hash.new(0)
-    array = phrase.downcase.scan(/\w+'\w|\w+/).flatten
-    array.each do |word|
-      @word_count[word] += 1
+    @array = phrase.downcase.scan(/\w+'\w|\w+/).flatten
+    @count = Hash.new(0)
+  end
+
+  def word_count
+    @array.each do |word|
+      @count[word] += 1
     end
+    @count
   end
 end
 
