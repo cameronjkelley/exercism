e1e3b387dc4804764ea67b50e08e95964f97043e
Cameron Kelley <i.am.cjk@gmail.com>
2018-07-26 16:27:45 -0400

    Update nth_prime.rb

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -3,23 +3,22 @@ class Prime
     raise ArgumentError if n < 1
 
     primes = [2, 3]
-    current = 3
+    current = primes.last
 
     while n > primes.length
       current += 2
-       if self.is_prime?(current)
-         primes << current
-       end
+      primes << current if is_prime?(current)
     end
 
     primes[n - 1]
   end
 
-  def self.is_prime?(x)
-    (3..Math.sqrt(x)).step(2).all? do |y|
-      x % y != 0
+  private
+    def self.is_prime?(x)
+      (3..Math.sqrt(x)).step(2).all? do |y|
+        x % y != 0
+      end
     end
-  end
 end
 
 module BookKeeping
