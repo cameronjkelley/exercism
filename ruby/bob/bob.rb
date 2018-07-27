7f45c30ce8484854a42d69759b06bacb45f7453d
Cameron Kelley <i.am.cjk@gmail.com>
2018-07-27 17:43:28 -0400

    Update bob.rb

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,34 +1,35 @@
 class Bob
   def self.hey(remark)
     stripped = remark.strip
-    if self.asking_forcefully?(stripped)
+    if asking_forcefully?(stripped)
       "Calm down, I know what I'm doing!"
-    elsif self.asking?(stripped)
+    elsif asking?(stripped)
       "Sure."
-    elsif self.yelling?(stripped)
+    elsif yelling?(stripped)
       "Whoa, chill out!"
-    elsif self.silent?(stripped)
+    elsif silent?(stripped)
       "Fine. Be that way!"
     else
       "Whatever."
     end
   end
 
-  def self.asking?(remark)
-    remark[-1] == "?"
-  end
+  private
+    def self.asking?(remark)
+      remark[-1] == "?"
+    end
 
-  def self.asking_forcefully?(remark)
-    remark[-1] == "?" && remark == remark.upcase && /[A-Z]/.match?(remark)
-  end
+    def self.asking_forcefully?(remark)
+      remark[-1] == "?" && remark == remark.upcase && /[A-Z]/.match?(remark)
+    end
 
-  def self.yelling?(remark)
-    remark == remark.upcase && /[A-Z]/.match?(remark)
-  end
+    def self.yelling?(remark)
+      remark == remark.upcase && /[A-Z]/.match?(remark)
+    end
 
-  def self.silent?(remark)
-    remark.empty?
-  end
+    def self.silent?(remark)
+      remark.empty?
+    end
 end
 
 module BookKeeping
