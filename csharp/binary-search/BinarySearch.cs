b85ee003ef2900fe561e03cf77f49fef31b04243
Cameron Kelley <i.am.cjk@gmail.com>
2018-12-11 16:21:23 -0500

    First Iteration

---
+++ csharp/binary-search/BinarySearch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+        int left = 0,
+            right = input.Length - 1;
+
+        while (left <= right)
+        {
+            int m = (int)Math.Floor((decimal)((left + right) / 2));
+
+            if (value > input[m])
+                left = m + 1;
+            else if (value < input[m])
+                right = m - 1;
+            else
+                return m;
+        }
+
+        return -1;
+    }
+}
