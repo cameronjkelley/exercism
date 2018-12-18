7d01b5aad671880f55a44ee6d8d79f848e9ff9a6
Cameron Kelley <i.am.cjk@gmail.com>
2018-12-18 13:08:20 -0500

    Add files via upload

---
+++ csharp/isbn-verifier/IsbnVerifier.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace IsbnVerifier
+{
+    public static class IsbnVerifier
+    {
+        public static bool IsValid(string number)
+        {
+            Regex regex = new Regex(@"[\dxX]");
+            List<int> numbers = new List<int>();
+
+            foreach (Match match in regex.Matches(number))
+            {
+                if ((match.Value == "X" || match.Value == "x") && 
+                    match.Index == number.Length - 1)
+                {
+                    numbers.Add(10);
+                }
+                else
+                {
+                    if (int.TryParse(match.Value, out int tryInt))
+                    {
+                        numbers.Add(tryInt);
+                    }
+                }
+            }
+
+            return numbers.Count == 10 && numbers.Select((x, i) => x * (10 - i)).Sum() % 11 == 0;
+        }
+    }
+}
