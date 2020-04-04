47f97144480e15275c081c6d2d71809e6aae6733
Cameron Kelley <i.am.cjk@gmail.com>
2020-04-04 16:13:02 -0400

    Update RomanNumerals.cs

--- csharp/roman-numerals/RomanNumerals.cs
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -1,25 +1,30 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 public static class RomanNumerals
 {
   private static Dictionary<int, string> Numerals = new Dictionary<int, string>()
   {
-    { 1000, "M" }, { 900, "CM" }, { 500, "D" }, { 400, "CD" }, { 100, "C" },
-    { 90, "XC" }, { 50, "L" }, { 40, "XL" }, { 10, "X" },
-    { 9, "IX" }, { 5, "V" }, { 4, "IV" }, { 1, "I" }
+    { 1000, "M" }, { 900, "CM" }, 
+    { 500, "D" }, { 400, "CD" }, 
+    { 100, "C" }, { 90, "XC" }, 
+    { 50, "L" }, { 40, "XL" }, 
+    { 10, "X" }, { 9, "IX" }, 
+    { 5, "V" }, { 4, "IV" }, 
+    { 1, "I" }
   };
 
   public static string ToRoman(this int input)
   {
-    string output = "";
+    StringBuilder output = new StringBuilder();
     foreach (var pair in Numerals)
     {
       while (input / pair.Key > 0)
       {
-        output += pair.Value;
+        output.Append(pair.Value);
         input -= pair.Key;
       }
     }
-    return output;
+    return output.ToString();
   }
 }
