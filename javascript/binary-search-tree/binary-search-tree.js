fa755a4546feec07cba69a5d3f91f14167d2daf6
ckelley <cameron.kelley@testsys.com>
2021-12-23 17:52:04 -0500

    formatting

--- javascript/binary-search-tree/binary-search-tree.js
+++ javascript/binary-search-tree/binary-search-tree.js
@@ -1,43 +1,43 @@
 export class BinarySearchTree {
   constructor(data) {
-    this._data = data;
-    this._left = null;
-    this._right = null;
+    this._data = data
+    this._left = null
+    this._right = null
   }
 
   get data() {
-    return this._data;
+    return this._data
   }
 
   get left() {
-    return this._left;
+    return this._left
   }
 
   get right() {
-    return this._right;
+    return this._right
   }
 
   insert(data) {
     if (data <= this._data) {
       if (this._left === null)
-        this._left = new BinarySearchTree(data);
+        this._left = new BinarySearchTree(data)
       else
-        this._left.insert(data);
+        this._left.insert(data)
     } else {
       if (this._right === null)
-        this._right = new BinarySearchTree(data);
+        this._right = new BinarySearchTree(data)
       else
-        this._right.insert(data);
+        this._right.insert(data)
     }
   }
 
   each(func) {
-    if (this._left !== null) {
+    if (this._left !== null) 
       this._left.each(func)
-    }
+
     func(this._data)
-    if (this._right !== null) {
+
+    if (this._right !== null) 
       this._right.each(func)
-    }
   }
 }
